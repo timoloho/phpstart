e49e8a572866fdf804da355ec40dc59ae722b4c5
timoloho <92794268+timoloho@users.noreply.github.com>
2022-09-20 11:44:41 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'd110472.mysql.zonevs.eu'
+$db = 'd110472_books'
+$user = 'd110472sa424272'
+$pass = 'Pajuvaints1'
+$charset = 'utf8mb4'
+
+$dsn = "mysql.host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE
+    PDO::ATTR_DEFAULT_FETCH_MODE
+]
